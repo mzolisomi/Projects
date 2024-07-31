37a1326ff24a6de22489aebf67e3dd458172236e
Mzoli Somi <202249599@ufh.ac.za>
2024-07-31 15:16:55 +0200

    oush

---
+++ DOM.js
@@ -0,0 +1,11 @@
+/**
+ * @Author: Your name
+ * @Date:   2024-07-31 15:07:54
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2024-07-31 15:15:42
+ */
+'use strict';
+const body = document.getElementsByTagName('body')
+const div = createElement('div')
+
+body.appendChild("div");
